adf3c60edc210e06ff4f86123cab7cd6f8e724e7
laurensruiz <laurenruiz97@yahoo.com>
2022-07-17 19:15:17 -0400

    created a nav bar that shows up on click

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import './NavbarStyles.css'
+
+function Navbar() {
+    const[click, setClick] = useState(false)
+    const handleClick = () => {
+        setClick(!click)
+    }
+  return (
+    <div className='header'>
+       <Link to='/'><h1>GLX TRVL</h1></Link>
+       <ul className={click ? 'nav-menu active': 'nav-menu'}>
+        <li>
+            <Link to='/'>Home</Link>
+        </li>
+        <li>
+            <Link to='/pricing'>Pricing</Link>
+        </li>
+        <li>
+            <Link to='/training'>Training</Link>
+        </li>
+        <li>
+            <Link to='/contact'>Contact</Link>
+        </li>
+       </ul>
+       <div className='hamburger' onClick={handleClick}>
+        {click ? (<FaTimes size={20} style ={{color: '#fff'}}/>):(<FaBars size={20} style ={{color: '#fff'}}/>)}
+       </div>
+    </div>
+  )
+}
+
+export default Navbar
