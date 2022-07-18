7069180a099862ce2af2530fd1f911959c783c35
laurensruiz <laurenruiz97@yahoo.com>
2022-07-17 21:17:11 -0400

    finished styling prcing page

---
+++ src/components/PricingCards.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './PricingStyles.css'
+
+function PricingCards() {
+  return (
+    <div className='pricing'>
+        <div className='card-container'>
+            <div className='card'>
+                <h3>- Basic -</h3>
+                <span className='bar'></span>
+                <p className='btc'>1BTC</p>
+                <p>- 3 days -</p>
+                <p>- Views -</p>
+                <p>- Featured -</p>
+                <p>- Private Quarters -</p>
+                <Link to ='/contact' className='btn'>Book</Link>
+            </div>
+            <div className='card'>
+                <h3>- Suite -</h3>
+                <span className='bar'></span>
+                <p className='btc'>1BTC</p>
+                <p>- 3 days -</p>
+                <p>- Views -</p>
+                <p>- Featured -</p>
+                <p>- Private Quarters -</p>
+                <Link to ='/contact' className='btn'>Book</Link>
+            </div>
+            <div className='card'>
+                <h3>- Executive -</h3>
+                <span className='bar'></span>
+                <p className='btc'>1BTC</p>
+                <p>- 3 days -</p>
+                <p>- Views -</p>
+                <p>- Featured -</p>
+                <p>- Private Quarters -</p>
+                <Link to ='/contact' className='btn'>Book</Link>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default PricingCards
--- src/routes/Pricing.js
+++ src/routes/Pricing.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../components/Footer'
 import HeroImage from '../components/HeroImage'
 import Navbar from '../components/Navbar'
+import PricingCards from '../components/PricingCards'
 
 function Pricing() {
   return (
     <div>
         <Navbar/>
         <HeroImage heading='PRICING' text='Choose your trip.'/>
+        <PricingCards/>
         <Footer/>
     </div>
   )
